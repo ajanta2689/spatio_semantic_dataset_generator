8299653cd64dc921ad31d76990f1f8a984565584
Patrick <patrick.westphal@informatik.uni-leipzig.de>
2022-08-30 10:46:55 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='spatio_semantic_dataset_generator',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='patrick',
+    author_email='',
+    description='',
+    install_requires=[
+        'rdflib==6.2.0',
+    ]
+)
